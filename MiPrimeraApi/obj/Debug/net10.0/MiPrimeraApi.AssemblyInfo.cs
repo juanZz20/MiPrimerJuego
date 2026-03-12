4cc5e052b6a6772e9475b4b3ef361d97c94bedfb
Juan <mar82697@gmail.com>
2026-03-12 00:04:56 -0500

    metodos get y post (otra vez XD)

--- MiPrimeraApi/obj/Debug/net10.0/MiPrimeraApi.AssemblyInfo.cs
+++ MiPrimeraApi/obj/Debug/net10.0/MiPrimeraApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiPrimeraApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73c21018c9ec514c1c6da077e42daad33a71d3af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb1556374b2b17967fdcfb62bc27aba60fdb509")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiPrimeraApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiPrimeraApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
